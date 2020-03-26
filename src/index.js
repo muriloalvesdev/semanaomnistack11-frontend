72cd189cb6bd85c673de4ce1c04b323a6746bd9b
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-03-26 01:19:47 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-const express = require('express');
-const app = express();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,5 +8,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-const port = process.env.PORT || 3000;
-app.listen(port); 
