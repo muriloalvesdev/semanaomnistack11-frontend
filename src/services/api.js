92042a97e884aa57c0fb3bfaafecba4c90c482ee
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-06-14 10:59:43 -0300

    modificando baseurl para apontar para a aws

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://backend-bethehero-java.herokuapp.com/'
+    baseURL: "http://18.230.150.45:8080"
 });
 
 export default api;
