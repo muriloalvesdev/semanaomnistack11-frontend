c22733a3ce070b2331d718a8813581baf35e07cb
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-06-16 23:14:05 -0300

    api deve apontar para o heroku :p

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    //baseURL: 'https://backend-bethehero-java.herokuapp.com/'
-    baseURL: 'http://localhost:8080/'
+    baseURL: 'https://backend-bethehero-java.herokuapp.com/'
+    //baseURL: 'http://localhost:8080/'
 });
 
 export default api;
